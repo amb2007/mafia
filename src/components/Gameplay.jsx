d4fc0560ac297ac8d208e9beb8b326fe44701ac2
Joaquin Ignacio Fritz Carrizo <d48125437@alumnos.itr.edu.ar>
2024-10-21 11:23:49 -0300

    jugadores por rol

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 
 const Gameplay = ({ players }) => {
-    return (
-        <div className="Gameplay-container">
-            <h3>Turno de los jugadores</h3>
-            <div className="cards">
-                {players.map((player) => (
-                    <div key={player.id} className="card">
-                        <h4>{player.name}</h4>
-                        <img 
-                            src={`images/${player.role}.png`} 
-                            width="20px" 
-                            alt={`Imagen de rol de ${player.name}`} 
-                        />
-                        <p>Rol: {console.log(player.role)}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const arr = [1, 2, 3, 4, 5, 6];
+    let index = 0;
+  const showPlayers = () => {
+    for (let index = 0; index < arr.length; index++) {
+      id = index;
+      <>Turno de {players[id].name}</>;
+    }
+  };
+  return (
+    <div className="Gameplay-container">
+      <h3>Turno de los jugadores</h3>
+      <div className="cards"></div>
+      <div key={players[index].id} className="card">
+        <h4>{players[index].name}</h4>
+        <img
+          src={`images/${players[index].role}.png`}
+          width="200px"
+          alt={`Imagen de rol de ${players[index].name}`}
+        />
+        <p>Rol: {console.log(players[index].role)}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Gameplay;
